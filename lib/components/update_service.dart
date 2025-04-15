849fc6cf8eeba9e7771aa986e4301c716ca7b6be
FlamingWater35 <flamingwater69420@gmail.com>
2025-04-15 20:13:40 +0300

    Update repo references

--- lib/components/update_service.dart
+++ lib/components/update_service.dart
@@ -30,7 +30,7 @@ class UpdateService {
   static final _log = Logger('UpdateService');
 
   static const String _githubOwner = 'FlamingWater35';
-  static const String _githubRepo = 'notes_app';
+  static const String _githubRepo = 'NotesApp';
   static const String _updateFilePrefsKey = 'downloaded_update_path';
 
   static Future<UpdateInfo> checkForUpdate() async {
